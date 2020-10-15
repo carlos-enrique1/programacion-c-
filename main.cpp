d6e7a000af6e003a721eecccd149cb67a4216990
Aula <Aula@EQUIP079586.xunta.local>
2020-10-15 13:12:41 +0200

    version 2

--- main.cpp
+++ main.cpp
@@ -4,22 +4,17 @@
 
 int main() { // sin argumentos
 
-
    // creamos una instancia de la clase Suma, o lo que es lo mismo,
    // una variable del tipo Suma,
 
    Suma s; 
-   
-
+ 
  s.cargar1();
  s.cargar2();
  s.operar();
  s.mostrar_resultado();
 
-
-
    Resta r;
-
 
  r.cargar1();
  r.cargar2();
